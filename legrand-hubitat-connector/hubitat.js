828ee44c87ccfa784aecb2c0e647aa3642a3e5e6
pharrison5 <59491255+pharrison5@users.noreply.github.com>
2025-11-29 23:41:32 -0700

    Update hubitat.js

--- legrand-hubitat-connector/hubitat.js
+++ legrand-hubitat-connector/hubitat.js
@@ -1,13 +1,13 @@
 const axios = require('axios');
-require('dotenv').config();
+
+// If Hubitat supports discovery, add similar logic here
+const HUBITAT_BASE_URL = 'http://hubitat.local/apps/api/your_app_id';
+const HUBITAT_TOKEN = 'your_hubitat_token';
 
 async function sendToHubitat(deviceId, command) {
-  try {
-    const url = `${process.env.HUBITAT_BASE_URL}/devices/${deviceId}/${command}?access_token=${process.env.HUBITAT_TOKEN}`;
-    await axios.get(url);
-    console.log(`Sent command "${command}" to Hubitat device ${deviceId}`);
-  } catch (error) {
-    console.error(`Failed to send command to Hubitat device ${deviceId}:`, error.message);
-  }
+  const url = `${HUBITAT_BASE_URL}/devices/${deviceId}/${command}?access_token=${HUBITAT_TOKEN}`;
+  await axios.get(url);
+  console.log(`Sent command "${command}" to Hubitat device ${deviceId}`);
 }
 
+module.exports = { sendToHubitat };
